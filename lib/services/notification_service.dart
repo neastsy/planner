9a390fbf5055c092882508d303566396608b4ace
NEASTsy <srknyrlmz14@gmail.com>
2025-07-07 02:25:30 +0300

    refactor logo

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -22,7 +22,7 @@ class NotificationService {
 
     // 2. Android için başlatma ayarları
     const AndroidInitializationSettings androidSettings =
-        AndroidInitializationSettings('@mipmap/launcher_icon');
+        AndroidInitializationSettings('@drawable/ic_notification');
 
     // 3. iOS için başlatma ayarları
     const DarwinInitializationSettings iosSettings =
